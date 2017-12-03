70087d5e52455882d9c501f8da50a2b4d7ab317b
dyy <932331954@qq.com>
2017-12-04 00:29:02 +0800

    实现append方法

--- praction/jquery/demo.js
+++ praction/jquery/demo.js
@@ -2,4 +2,34 @@
 var myIpt = $$('#ipt')
 var myIpt2 = $('#ipt')
 var mySpan = $$('#myspan')
-myIpt.val('3333').setColor('#0f0').consoleVal()
+// myIpt.val('3333').setColor('#0f0').consoleVal()
+
+myIpt.addEventHander('change',function(){
+    alert(this.value)
+})
+
+// $$('#btn').click(function(){
+//     alert('111')
+// })
+
+
+$('#btn').on('click',function(){
+    alert('111')
+})
+
+// var span = $('<div><span>22133</span></div><div><span>22133</span></div>')
+// $('#mydiv').append(span)
+
+// var span1 = document.createElement('span')
+// span1.innerText = '33333'
+// span1.setAttribute('ID','myspan1')
+// document.querySelector('#mydiv').appendChild(span1)
+
+// document.querySelector('#mydiv').innerHTML = '<div><span>22133</span></div><div><span>22133</span></div>'
+
+// $$('#mydiv').append('<div><span>2222</span></div><div><span>2222</span></div>')
+
+var dom = $('<div><span>2222</span></div><div><span>2222</span></div>')
+
+$$('#mydiv').append(dom[0])
+$$('#mydiv').prepend(dom[0])
--- praction/jquery/myJquery.js
+++ praction/jquery/myJquery.js
@@ -10,6 +10,7 @@
 
     myJquery.fn = myJquery.prototype = {
         init: function(selector,context) {
+
             //3. 类数组对象结构
             var domElements = document.querySelectorAll(selector)
             this.length = domElements.length
@@ -28,27 +29,65 @@
     myJquery.prototype.init.prototype = myJquery.fn
 
     // 4. 链式调用
-    myJquery.fn.val = function (value) {
-        this[0].value = value
-        return this
-    }
+    // myJquery.fn.val = function (value) {
+    //     this[0].value = value
+    //     return this
+    // }
 
-    myJquery.fn.setColor = function ( color ) {
-        this[0].style.color = color
-        return this
-    }
+    // myJquery.fn.setColor = function ( color ) {
+    //     this[0].style.color = color
+    //     return this
+    // }
     
     // 5.1 插件接口
-    myJquery.fn.extend = myJquery.extend = function (extendObj) {
+    myJquery.fn.extend = function (extendObj) {
         Object.assign(this,extendObj)
-    }
+    };
 
     // 5.2 增加插件
-    myJquery.fn.extend ({
-        consoleVal: function() {
-            alert(this[0].value)
+    // myJquery.fn.extend ({
+    //     consoleVal: function() {
+    //         alert(this[0].value)
+    //     }
+    // })
+
+    (function(){
+        var event = {
+            addEventHander: function(type,fn) {
+                if(document.addEventListener) {
+                    this[0].addEventListener(type,fn)
+                }else {
+                    this[0].attchEvent('on'+type,fn)
+                }
+            },
+            click:function(fn) {
+                this.addEventHander('click',fn)
+            }
         }
-    })
+    
+        myJquery.fn.extend(event)
+    })()
+
+
+    var dom={
+        append:function(str){
+            if(typeof str === 'string') {
+                this[0].innerHTML = str
+            } else if( str.nodeType ) {
+                this[0].appendChild(str)
+            } 
+        },
+        prepend: function(str) {
+            if(typeof str==='string'){
+            
+            }else{
+                this[0].insertBefore(str,someNode.firstChild);
+            }
+        }
+    }
+    myJquery.fn.extend(dom)
+
+
 
     window.$$ = myJquery;
 
