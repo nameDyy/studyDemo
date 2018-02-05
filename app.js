5ce88142ea2ecce2e851d5eb35a3c4bb29664309
dy <932331954@qq.com>
2018-02-05 20:29:37 +0800

    vue练习

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'praction')));
+app.use(express.static(path.join(__dirname, 'vue')));
 
 app.use('/', index);
 app.use('/users', users);
